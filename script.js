b47f43e9ff3a74f6c3f84787cc95f8a18ba6b9af
Vlad28041992 <mr.n-vlad@yandex.ru>
2025-07-05 21:05:54 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function sendTask(task) {
-  Telegram.WebApp.sendData(task);  // Отправить данные в бота
-  Telegram.WebApp.close();         // Закрыть WebApp
-}
-
-Telegram.WebApp.expand(); // Сделать окно на весь экран
